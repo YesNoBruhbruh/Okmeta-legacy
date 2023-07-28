77f118a7dfe7908367d25bae4708b32aae0535e4
BuildTools <unconfigured@null.spigotmc.org>
2023-07-28 21:32:41 +0800

    this will be the new command system. which is just nothing

--- src/main/kotlin/com/maanraj514/Okmeta.kt
+++ src/main/kotlin/com/maanraj514/Okmeta.kt
@@ -49,7 +49,7 @@ abstract class Okmeta : JavaPlugin() {
         Bukkit.getConsoleSender().sendMessage("&c[&cOkmeta&c]&c: &cThe Core has stopped!".toColor())
     }
 
-    private fun registerListeners(vararg listeners: Listener?) {
+    fun registerListeners(vararg listeners: Listener?) {
         val pm = Bukkit.getPluginManager()
 
         for (listener in listeners) {
@@ -57,7 +57,7 @@ abstract class Okmeta : JavaPlugin() {
         }
     }
 
-    private fun registerCommands(command: Command) {
+    fun registerCommands(command: Command) {
         try {
             val bukkitCommandMap: Field = Bukkit.getServer().javaClass.getDeclaredField("commandMap")
             bukkitCommandMap.isAccessible = true
